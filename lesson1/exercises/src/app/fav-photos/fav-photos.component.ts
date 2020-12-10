dec6327d0e9a04da97e7a4adbb2b9114313739f1
Sandy Hadley <sandymariehadley@gmail.com>
2020-12-10 12:44:51 -0600

    Changed movies, chores, and photos html and css, added photos

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Photo Favorites';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://images.pexels.com/photos/3626111/pexels-photo-3626111.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260';
+  image3 = 'https://images.pexels.com/photos/668867/pexels-photo-668867.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260';
 
   constructor() { }
 
